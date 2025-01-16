e3a352324f2b42414ecaa117075eb9bf76db6c28
arimu <arimurahiroaki40@gmail.com>
2025-01-16 22:48:36 +0900

    jsonサーバーを構築

--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -1,44 +1,54 @@
 import Link from "next/link";
 import Image from "next/image";
 import React from "react";
+import { Article } from "@/types";
 
-const ArticleList = () => {
+type ArticleListProps = {
+  articles: Article[];
+};
+
+const ArticleList = ({ articles }: ArticleListProps) => {
   return (
     <div>
-      <article className="flex flex-col shadow my-4">
-        <Link href="/articles/page">
-          <Image
-            src="https://picsum.photos/800"
-            alt=""
-            width={1280}
-            height={300}
-          />
-        </Link>
-        <div className="bg-white flex flex-col justify-start p-6">
-          <Link
-            href="#"
-            className="text-blue-700 text-sm font-bold uppercase pb-4"
-          >
-            Technology
+      {articles.map((article) => (
+        <article className="flex flex-col shadow my-4" key={article.id}>
+          <Link href={`articles/${article.id}`} className="hover: opacity-75">
+            <Image
+              src="https://picsum.photos/800"
+              alt=""
+              width={1280}
+              height={300}
+            />
           </Link>
-          <Link
-            href="#"
-            className="text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4"
-          >
-            Next.js 勉強中
-          </Link>
-          <p className="text-sm pb-3 text-slate-900">
-            By hiro, Published on 2025/1
-          </p>
-          <Link href="#" className="pb-6 text-slate-900">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse varius enim in eros elementum tristique.
-            </p>
-          </Link>
-          <Link href="#" className="uppercase text-pink-800 hover:text-black">続きを読む</Link>
-        </div>
-      </article>
+          <div className="bg-white flex flex-col justify-start p-6">
+            <Link
+              href="#"
+              className="text-blue-700 text-sm font-bold uppercase pb-4"
+            >
+              Technology
+            </Link>
+            <Link
+              href={`articles/${article.id}`}
+              className="text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4"
+            >
+              {article.title}
+            </Link>
+            <p className="text-sm pb-3 text-slate-900">{article.createdAt}</p>
+            <Link
+              href={`articles/${article.id}`}
+              className="pb-6 text-slate-900"
+            >
+              <p>{article.content}</p>
+            </Link>
+            <Link
+              href={`articles/${article.id}`}
+              className="uppercase text-pink-800 hover:text-black"
+            >
+              続きを読む
+            </Link>
+          </div>
+        </article>
+      ))}
     </div>
   );
 };
